26fe1735144329e2073a1eaba956033a95c67584
Sadicius <124639760+Sadicius@users.noreply.github.com>
2024-11-11 03:19:46 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,21 @@
 fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
 
 author 'Nevera Development'
 description 'OX Debug Menu'
 version '1.0.0'
 
-shared_scripts { '@ox_lib/init.lua' }
-client_scripts { 'client/client.lua' }
+shared_scripts {
+  '@ox_lib/init.lua'
+}
+
+client_scripts {
+  'client/client.lua'
+}
+
+files {
+  'locales/*.json'
+}
+
+lua54 'yes'
